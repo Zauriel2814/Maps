0d007d45d0e9d9327ea1844a1016b264eb69f20f
othmanbrahman <othmanbinrahman@gmail.com>
2022-11-19 14:14:56 +0800

    add layers

--- script.js
+++ script.js
@@ -10,4 +10,62 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
 }).addTo(map);
+loadData();
+loadTracks();
 
+async function loadData() {
+    // important: axios.get requires a URL
+    // in this case we are using a relative URL
+    let response = await axios.get("cycling-path.geojson");
+
+    // display the content of the geojson file on the map
+    // response.data holds the actual data from the geojson file
+    // the second paramter of L.geoJson is a configuration object
+    const cyclingLayer = L.geoJson(response.data, {
+        // onEachFeature is a fixed function name from Leaflet
+        // it is called for each feature in response.data
+        onEachFeature: function(feature, layer) {
+            // feature paramter to the data of the feature
+            console.log(feature);
+
+            // layer parameter is the shape, line or marker etc. that will be added to the map
+            layer.bindPopup(feature.properties.Description);
+        }
+    })
+
+    // add the cycling layer to the map
+    cyclingLayer.addTo(map);
+
+    cyclingLayer.setStyle({
+        color: 'red'
+    })
+}   
+
+async function loadTracks() {
+     // important: axios.get requires a URL
+    // in this case we are using a relative URL
+    let tracksData = await axios.get("npark-tracks.geojson");
+
+    // display the content of the geojson file on the map
+    // response.data holds the actual data from the geojson file
+    // the second paramter of L.geoJson is a configuration object
+    const tracksLayer = L.geoJson(tracksData.data, {
+        // onEachFeature is a fixed function name from Leaflet
+        // it is called for each feature in response.data
+        onEachFeature: function(feature, layer) {
+            // feature paramter to the data of the feature
+            console.log(feature);
+
+            // layer parameter is the shape, line or marker etc. that will be added to the map
+            layer.bindPopup(feature.properties.Description);
+        }
+    })
+
+    // add the cycling layer to the map
+    tracksLayer.addTo(map);
+
+    tracksLayer.setStyle({
+        color: 'blue'
+    })
+
+}
